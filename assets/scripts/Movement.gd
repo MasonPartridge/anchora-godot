354bbf20f607208c6b6cb5310cd36faca4e77552
SudoNoun <116114321+MasonPartridge@users.noreply.github.com>
2024-06-09 18:00:51 -0700

    feat(sprites): Remaster ship

--- assets/scripts/Movement.gd
+++ assets/scripts/Movement.gd
@@ -11,12 +11,12 @@ func _process(delta):
 	var velocity = Vector2.ZERO
 	
 	if travel_distance > 0:
-		velocity = Vector2.LEFT.rotated(rotation) * speed
+		velocity = Vector2.UP.rotated(rotation) * speed
 		travel_distance -= speed * delta
 		if travel_distance <= 0:
 			reached_waypoint.emit()
 	position += velocity * delta
 
 func _on_waypoints_new_waypoint(waypoint_position):
-	rotation = waypoint_position.angle_to_point(position)
+	rotation = waypoint_position.angle_to_point(position) - PI/2
 	travel_distance = position.distance_to(waypoint_position)
